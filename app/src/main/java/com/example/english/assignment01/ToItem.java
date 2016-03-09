bcd6c00b437b734ac0ca8af88f26fda08c80bc07
unknown <english@gcc-pc6030.student.gcc.ie>
2016-03-09 13:03:46 +0000

    Final Milestone 6

---
+++ app/src/main/java/com/example/english/assignment01/ToItem.java
@@ -0,0 +1,31 @@
+package com.example.english.assignment01;
+
+/**
+ * Created by english on 09/03/2016.
+ */
+public class ToItem {
+
+    boolean checked;
+    String data;
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public ToItem(boolean checked, String dat){
+        data = dat;
+        this.checked = checked;
+    }
+}
